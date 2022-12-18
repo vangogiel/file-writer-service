df422399f566d713ed0c85b935c567a2fa7e3b4f
vangogiel <vangogiel@hotmail.co.uk>
2022-12-18 12:32:56 +0000

    Add rate limiting actor

---
+++ src/test/scala/myraindrop/exercise/actor/RequestsRateLimitingActorSpec.scala
@@ -0,0 +1,67 @@
+package myraindrop.exercise.actor
+
+import akka.actor.ActorSystem
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
+import akka.util.Timeout
+import myraindrop.exercise.actor.RequestsRateLimitingActor.{ LimitReached, Allowed, RequestResource }
+import org.scalatest.wordspec.AnyWordSpecLike
+
+import scala.collection.immutable.Map
+import scala.concurrent.duration.DurationInt
+
+class RequestsRateLimitingActorSpec extends ScalaTestWithActorTestKit with AnyWordSpecLike {
+  implicit lazy val actorSystem: ActorSystem = ActorSystem("test-actor")
+  override implicit val timeout: Timeout = Timeout(5.seconds)
+
+  "RequestsRateActor" should {
+    "respond with RequestAllowed to one request" in {
+      val testProbe = testKit.createTestProbe[RequestsRateLimitingActor.Response]()
+      val subject = testKit.spawn(RequestsRateLimitingActor.apply(Map.empty))
+
+      subject ! RequestResource("mockRequestId", testProbe.ref)
+
+      testProbe.expectMessageType[Allowed]
+    }
+
+    "respond with RequestAllowed to two identical requests within a second" in {
+      val testProbe = testKit.createTestProbe[RequestsRateLimitingActor.Response]()
+      val subject = testKit.spawn(RequestsRateLimitingActor.apply(Map()))
+
+      subject ! RequestResource("mockRequestId", testProbe.ref)
+      subject ! RequestResource("mockRequestId", testProbe.ref)
+
+      testProbe.expectMessageType[Allowed]
+      testProbe.expectMessageType[Allowed]
+    }
+
+    "respond with LimitReached to three identical requests within a second" in {
+      val testProbe = testKit.createTestProbe[RequestsRateLimitingActor.Response]()
+      val subject = testKit.spawn(RequestsRateLimitingActor.apply(Map()))
+
+      val millsStart = System.currentTimeMillis()
+      subject ! RequestResource("mockRequestId", testProbe.ref)
+      subject ! RequestResource("mockRequestId", testProbe.ref)
+      subject ! RequestResource("mockRequestId", testProbe.ref)
+      val millsEnd = System.currentTimeMillis()
+
+      assert(millsEnd - millsStart < 1000)
+      testProbe.expectMessageType[Allowed]
+      testProbe.expectMessageType[Allowed]
+      testProbe.expectMessageType[LimitReached]
+    }
+
+    "respond with RequestAllowed to two identical requests within a second and a third after" in {
+      val testProbe = testKit.createTestProbe[RequestsRateLimitingActor.Response]()
+      val subject = testKit.spawn(RequestsRateLimitingActor.apply(Map()))
+
+      subject ! RequestResource("mockRequestId", testProbe.ref)
+      subject ! RequestResource("mockRequestId", testProbe.ref)
+      Thread.sleep(1000)
+      subject ! RequestResource("mockRequestId", testProbe.ref)
+
+      testProbe.expectMessageType[Allowed]
+      testProbe.expectMessageType[Allowed]
+      testProbe.expectMessageType[Allowed]
+    }
+  }
+}
